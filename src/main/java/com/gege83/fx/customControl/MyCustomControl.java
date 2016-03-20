2f85bdb7fb2b6e90788da8558d9025e943cd05b0
James <denvir@marshall.edu>
2016-03-20 10:54:05 -0400

    Fixed Spring configuration, custom component fxml defintion, custom component loading, various other errors.

--- src/main/java/com/gege83/fx/customControl/MyCustomControl.java
+++ src/main/java/com/gege83/fx/customControl/MyCustomControl.java
@@ -4,16 +4,17 @@ import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.HBox;
 
-@Configuration
+@Component
 public class MyCustomControl extends HBox {
 
 	private final SomeService service;
@@ -28,8 +29,12 @@ public class MyCustomControl extends HBox {
 	public MyCustomControl(SomeService service, ApplicationContext applicationContext) {
 		this.service = service;
 		try {
-			SpringFXMLLoader.create().applicationContext(applicationContext)
-					.location(getClass().getResource("MyCustomControl.fxml")).load();
+//			SpringFXMLLoader.create().applicationContext(applicationContext)
+//					.location(getClass().getResource("MyCustomControl.fxml")).load();
+		    FXMLLoader loader = new FXMLLoader(getClass().getResource("MyCustomControl.fxml"));
+		    loader.setController(this);
+		    loader.setRoot(this);
+		    loader.load();
 		} catch (final IOException e) {
 			e.printStackTrace();
 		}
@@ -39,7 +44,7 @@ public class MyCustomControl extends HBox {
 		return onClickMeHandler.get();
 	}
 
-	public void setOnClickMeHandler(EventHandler<ActionEvent> onClickMe) {
+	public void setOnClickMe(EventHandler<ActionEvent> onClickMe) {
 		this.onClickMeHandler.set(onClickMe);
 	}
 
--- src/main/java/com/gege83/fx/customControl/SomeService.java
+++ src/main/java/com/gege83/fx/customControl/SomeService.java
@@ -1,5 +1,8 @@
 package com.gege83.fx.customControl;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class SomeService {
 	public void doSomeThing() {
 		System.out.println("did some thing");
